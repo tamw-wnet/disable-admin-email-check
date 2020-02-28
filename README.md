f80f003f5312073bc6a1fa5a9cad9642561ee86c
Sonya Pinero <pineros@wnet.org>
2020-02-28 16:25:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# disable-admin-email-check
+Disable Stupid WordPress Admin Email Check
+
+As of WordPress 5.3, every 6 months any admin logging into the back end will be prompted to confirm the correct admin email.
+Particularly with a multi-site install, this is an annoying nag screen and pointless.
+Install and activate this plugin to disable this nag screen that should never have been put into WordPress core.
---
+++ disable-admin-email-check.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Disable Stupid Admin Email Check
+Plugin URI: https://github.com/tamw-wnet/disable-admin-email-check
+Description: Enable this plugin to completely disable the every-6-month admin email check that was stupidly added to WP Core in 5.3
+Version: 1.0
+Author: William Tam
+Author URI: https://ieg.wnet.org/
+License: GPL2
+
+------------------------------------------------------------------------
+Copyright WNET
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+*/
+
+// original source of this filter is in this blog post:
+// https://make.wordpress.org/core/2019/10/17/wordpress-5-3-admin-email-verification-screen/
+
+// yes this plugin is exactly one line of code
+add_filter( 'admin_email_check_interval', '__return_false' );
